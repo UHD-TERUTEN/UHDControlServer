71bde00472dfcb4cbac6e510728c35283b8fa67f
droplet92 <lhs1438@gmail.com>
2021-02-24 01:20:07 +0900

    심심해서 홈 화면 구현

--- ClientApp/pages/index.vue
+++ ClientApp/pages/index.vue
@@ -1,89 +1,60 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
+  <v-container>
+    <v-row justify="space-around">
+      <v-col justify="space-around">
+        <v-card>
+          <v-parallax height="400" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" />
+
+          <v-card-text>
+            <div class="font-weight-bold ml-8 mb-2">
+              Features
+            </div>
+
+            <v-timeline
+              align-top
+              dense
             >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+              <v-timeline-item
+                v-for="message in messages"
+                :key="message.time"
+                :color="message.color"
+                small
+              >
+                <div>
+                  <div class="font-weight-normal">
+                    <strong>{{ message.from }}</strong>
+                  </div>
+                  <div>{{ message.message }}</div>
+                </div>
+              </v-timeline-item>
+            </v-timeline>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  export default {
+    data: () => ({
+      messages: [
+        {
+          from: '파일 접근 차단 로그',
+          message: '에이전트 PC 환경에서 발생한 파일 접근 차단 로그 관리',
+          color: 'cyan lighten-3',
+        },
+        {
+          from: '화이트리스트',
+          message: '화이트리스트 버전 관리 및 에이전트 PC 배포',
+          color: 'orange darken-1',
+        },
+        {
+          from: '에이전트 시스템 로그',
+          message: '에이전트 PC 환경에서 발생한 시스템 로그 확인',
+          color: 'lime lighten-1',
+        },
+      ],
+    }),
   }
-}
 </script>
