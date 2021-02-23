243787377f2f0314e088f77620c15620a317e24f
gggg8657 <gggg8657@gmail.com>
2021-02-24 00:54:49 +0900

    whitelist

---
+++ ClientApp/pages/Whitelist.vue
@@ -0,0 +1,32 @@
+<template>
+
+<div>
+    <v-app>
+        <v-container>
+            <v-row>
+                <v-col
+                    cols="12"
+                    md="6"
+                >
+                    <v-textarea
+                        outlined
+                        name="input-7-4"
+                        label=""
+                        value="현재 화이트 리스트 버전 : 1.2.3
+현재 버전 업데이트 시각 : 2021.02.24 21:25
+마지막 배포 시각 :              2021.02.25 00:25."
+             
+          ></v-textarea>
+          <v-btn
+                color="primary"
+                elevation="1"
+              >
+                배포하기
+              </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-app>
+</div>
+
+</template>
