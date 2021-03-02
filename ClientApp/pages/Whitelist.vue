31ad608a444534ef5979822e179dd20b54fc4655
droplet92 <lhs1438@naver.com>
2021-03-03 00:29:34 +0900

    코드 정리 - 불필요한 DOM 삭제 - 변수이름 백엔드와  통일 - 스타일 통일 등

--- ClientApp/pages/Whitelist.vue
+++ ClientApp/pages/Whitelist.vue
@@ -1,31 +1,28 @@
 <template>
-    <div id="app">
-        <v-app id="inspire">
-            <div>
-                <v-alert text color="info">
-                    <h3 class="headline">
-                        White List
-                    </h3>
-                    <td v-for='info in object' 
-                    :key='info.name'></td>
-                    <div>
-                        <p> 현재 화이트 리스트 버전 : {{object.CurVer}}</p>
-                        <p> 현재 버전 업데이트 시각 : {{object.VerUT}}</p>
-                        <p> 마지막 배포 시각 : {{object.LastSpreadT}}</p>
-                    </div>
-                    <v-divider class="my-4 info" style="opacity: 0.22"></v-divider>
-                    <v-row align="center" no-gutters>
-                    <v-spacer></v-spacer>
-                    <v-col class="shrink">
-                    <v-btn color="info" outlined>
-                        배포하기
-                    </v-btn>
-                    </v-col>
-                    </v-row>
-                </v-alert>
-            </div>
-        </v-app>
-    </div>
+  <v-alert text color="info">
+    <v-row>
+      <v-col><h3 class="headline">Whitelist</h3></v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3">현재 화이트리스트 버전: </v-col>
+      <v-col>{{ object.version }}</v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3">현재 버전 업데이트 시각: </v-col>
+      <v-col>{{ object.lastUpdated }}</v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3">마지막 배포 시각: </v-col>
+      <v-col>{{ object.lastDistributed }}</v-col>
+    </v-row>
+    <v-divider class="my-4 info" style="opacity: 0.22" />
+    <v-row align="center" no-gutters>
+      <v-spacer />
+      <v-col class="shrink">
+        <v-btn color="primary" elevation="1"> 배포하기 </v-btn>
+      </v-col>
+    </v-row>
+  </v-alert>
 </template>
 
 <style>
@@ -33,15 +30,14 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       object: {
-            CurVer : "1.2.3",
-            VerUT : "2021.02.24 21:25",
-            LastSpreadT : "2021.02.25 00:24"
-        }
-      
-    }
-  }
-}
+        version: "1.2.3",
+        lastUpdated: "2021.02.24 21:25",
+        lastDistributed: "2021.02.25 00:24",
+      },
+    };
+  },
+};
 </script>
